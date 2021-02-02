caea30198737b9cb00764db3fb4a695e9e30ce50
SharonXal <rocksharon555@gmail.com>
2021-02-02 12:18:39 +0530

    Testing GIT

---
+++ src/com/company/Library.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Library {
+    int totalBooks, signUpDays;
+    boolean isSignedUp;
+    Library(int totalBooks, int signUpDays){
+        this.totalBooks = totalBooks;
+        this.signUpDays = signUpDays;
+        this.isSignedUp = false;
+    }
+}
