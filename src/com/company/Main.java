7641b51c4e3d9d9887c2974e8b4013375c389750
Ayush Kumar <eliasakeleven@gmail.com>
2021-02-05 01:53:19 +0530

    example 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,30 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     static int currentDay=0;
     static final int D=7;
 
     public static void main(String[] args) {
-        //Library lib0 = new Library(2,1,1)
+        ArrayList<Integer> bookID0 = new ArrayList<>();
+        bookID0.add(0);
+        ArrayList<Integer> bookID1 = new ArrayList<>();
+        bookID1.add(0);
+        bookID1.add(1);
+        bookID1.add(2);
+
+        Library lib0 = new Library(2,1,1,bookID0,0);
+        Library lib1 = new Library(3,3,1,bookID1,1);
+
+        ArrayList<Library> libraries = new ArrayList<>();
+        libraries.add(lib0);
+        libraries.add(lib1);
+        for(Library lib : libraries){
+            lib.signUp();
+        }
+
 
     }
 }
