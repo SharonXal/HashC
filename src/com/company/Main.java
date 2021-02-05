2ef2701b36e98347484e7606f1ccbee57e1ba604
Ayush Kumar <eliasakeleven@gmail.com>
2021-02-05 16:52:32 +0530

    example 1.4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,19 +13,33 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> bookID0 = new ArrayList<>();
         bookID0.add(0);
+        bookID0.add(1);
+        bookID0.add(2);
+        bookID0.add(3);
+        bookID0.add(4);
         ArrayList<Integer> bookID1 = new ArrayList<>();
         bookID1.add(0);
-        bookID1.add(1);
         bookID1.add(2);
+        bookID1.add(3);
+        bookID1.add(5);
 
-        Library lib0 = new Library(0,2,1, bookID0);
-        Library lib1 = new Library(1,3,1,bookID1);
+        Library lib0 = new Library(0,2,2, bookID0);
+        Library lib1 = new Library(1    ,3,1,bookID1);
 
         ArrayList<Library> libraries = new ArrayList<>();
         libraries.add(lib1);
         libraries.add(lib0);
         for(Library lib : libraries){
             lib.signUp();
+        }
+
+        System.out.println("Library 0 : ");
+        for(int x: lib0.getBooksForScanning()){
+            System.out.println(x+" ,");
+        }
+        System.out.println("Library 1 : ");
+        for(int x: lib1.getBooksForScanning()){
+            System.out.println(x+" ,");
         }
 
     }
