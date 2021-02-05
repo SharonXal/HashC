733bfd9c492d465c64449486b1dddb366db6da93
Ayush Kumar <eliasakeleven@gmail.com>
2021-02-06 01:43:59 +0530

    example 1.5

---
+++ src/com/company/Output.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+class Output {
+    void output(){
+            BufferedReader input=new BufferedReader(new InputStreamReader(System.in));
+
+            PrintWriter pw=new PrintWriter(new BufferedWriter(new FileWriter("File.txt"));
+            int A,Y,K,i;
+            for(i=0;i<A;i++)
+            {
+                pw.print("The ID of the library="+Y[i]\t);//the id of the library
+                pw.println("The no. of books to be scaned from library Y="+Y[i]);//The no. of books to be scaned
+                pw.println(/*fuction of the id's of the book to scan*/);
+            }
+                pw.close();
+                bw.close();
+                fw.close();
+        }
+        void display()
+        {
+        FileReader fr=new FileReader("File.txt");
+        BufferedReader bw=new BufferedReader(fr);
+        int A,Y,K;
+        while(br.readLine!=null)
+        {
+        A=br.readLine();
+        Y=br.readLine();
+        K=br.readLine();
+        }
+        br.close();
+        fr.close();
+        }
+}
